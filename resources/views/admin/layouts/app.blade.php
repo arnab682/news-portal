906a92235c460805eb3b731f55f5222caf9e2ade
arnab682 <arnab.das682@gmail.com>
2021-12-20 20:31:26 +0600

    designing admin dashboard

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,13 @@
+
+@include('admin.layouts.partials.header')
+
+        @include('admin.layouts.partials.sidebar')
+
+            @include('admin.layouts.partials.navbar')
+
+
+                @yield('content')
+
+
+
+@include('admin.layouts.partials.footer')
